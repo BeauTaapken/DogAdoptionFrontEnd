78de97b5d4c79262e28967d9a2637c7189d4bdea
BeauTaapken <beautaapken@me.com>
2020-03-10 09:56:11 +0100

    * Added a button and code to test adding a advert

--- src/views/Home.vue
+++ src/views/Home.vue
@@ -1,10 +1,14 @@
 <template>
-  <h1>Welcome to DogAdopt {{user.displayName}}</h1>
+  <v-container>
+    <h1>Welcome to DogAdopt {{ user.displayName }}</h1>
+    <v-btn @click="getdata">testing</v-btn>
+  </v-container>
 </template>
 
 <script lang="ts">
 import firebase from "firebase";
 import Vue from "vue";
+import axios from "axios";
 
 export default Vue.extend({
   name: "Home" as string,
@@ -15,6 +19,44 @@ export default Vue.extend({
   },
   mounted(): void {
     this.user = firebase.auth().currentUser;
+  },
+  methods: {
+    getdata() {
+      this.user
+        .getIdToken(/* forceRefresh */ true)
+        .then(function(idToken: string) {
+          console.log(idToken);
+          axios
+            .post(
+              "/advert/addadvert/",
+              {
+                UUID: {
+                  UUID: "uuid",
+                  Username: ""
+                },
+                image: "t",
+                title: "",
+                description: "",
+                breed: 1,
+                age: 15
+              },
+              {
+                headers: {
+                  id: idToken
+                }
+              }
+            ).then(response => {
+              console.log(response);
+            })
+            .catch(error => {
+              console.log(error);
+            });
+        })
+        .catch((error: any) => {
+          // Handle error
+          console.log(error);
+        });
+    }
   }
 });
 </script>
